6310f8a21a3f9b285aa006c58cc5e5aef23b4f5c
BeJoys8581 <beambeambenz81@gmail.com>
2023-10-13 18:47:22 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import HeaderDD from './views/HeaderDD.vue';
+import FooterDD from '@/views/FooterDD.vue'
+import MainPage from './views/MainPage.vue';
+
+</script>
+
+<template>
+    
+<HeaderDD/>
+
+
+<MainPage/>
+
+
+<FooterDD/>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HeaderDD.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg text-uppercase" id="mainNav" >
+    <div class="container1">
+      <a class="navbar-brand" href="/" style="color: rgb(0, 0, 0); display: flex; text-align: center; font-size: 50px;">
+                                                Joy-Weather ☁️
+      </a>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ms-auto">
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<style>
+
+.container1{
+  display: flex;
+  margin-left: 655px;
+}
+
+</style>
